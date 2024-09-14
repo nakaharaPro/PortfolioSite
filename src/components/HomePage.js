bd20910bf51d1d926282d8832c2d488260c2560b
nakahara <marimo09300130@gmail.com>
2024-09-14 23:35:40 +0900

    パーティクル

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,112 +1,65 @@
-import React from 'react'
-import {Link } from "react-router-dom";
+import React from 'react';
+import ParticlesBackground from '../particle/ParticleBackground'; // 正しいパスに修正
 
-import reactImage from "../Images/react.png";
-import jsImage from "../Images/js.png";
-import firebaseImage from "../Images/firebase.jpg";
 import profileImage from "../Images/sibainu.jpg";
 
-const twitterCloneWeb=()=>{
+const twitterCloneWeb = () => {
   window.location.replace("https://twitter-clone-98280.web.app");
 }
-const pokemonAppWeb=()=>{
+const pokemonAppWeb = () => {
   window.location.replace("https://main.d1blt17lrj7j3g.amplifyapp.com/");
 }
 
 function HomePage() {
   return (
-    <div className="container text-center">
-      <h1>Nakahara Kenta</h1>
+    <div style={{ position: 'relative' }}>
+      {/* ParticlesBackgroundを追加 */}
+      <ParticlesBackground />
 
-      <img src={profileImage} className="profileImage" />
+      <div className="container text-center" style={{ position: 'relative', zIndex: 1 }}>
+        <h1>Nakahara Kenta</h1>
+        <img src={profileImage} className="profileImage" alt="Profile of Nakahara Kenta" />
+        <p>
+          中原健太と申します。
+          SES営業からキャリアをスタートし、Java,SpringBootを使用した業務システムの開発に従事。
+        </p>
 
-      <p>
-        中原健太と申します。
-        <br />
-        SES営業からキャリアをスタートし、ServiceNowやSpotfireに従事し、
-        現在ではJava,SpringBootを使用した業務システムの開発に従事しております。
-        <br /> 
-        HTML,CSS,JavaScript,React,Flutterを主に自己研鑽として努めております。
-      </p>
-
-      <section class="page-section" id="services">
-        <div className="service">
-          <div class="text-center">
-            <h2 class="section-heading text-uppercase">PORTFOLIO</h2>
-            <h3 class="section-subheading text-muted mb-5">
-              私が作った作品一覧です
-            </h3>
-          </div>
-          <div class="row text-center">
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                  {/* ツイッタクローンのポケモン図鑑アイコンリンク */}
-                <div className="pokekemonIcon">
-              <i class="fa-brands fa-stack-overflow fa-xl" onClick={pokemonAppWeb}></i>
-                </div>
-
-              </span>
-              <h4 class="my-3">ポケモン図鑑</h4>
-              <p class="text-muted">
-                pokemonAPIをからJsonデータを取得し、取得したデータをReactで画面に反映させております。
-                画面下のボタンで画面遷移ができます。
-              </p>
+        <section className="page-section" id="services">
+          <div className="service">
+            <div className="text-center">
+              <h2 className="section-heading text-uppercase">PORTFOLIO</h2>
+              <h3 className="section-subheading text-muted mb-5">私が作った作品一覧です</h3>
             </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                {/* ツイッタクローンのアイコンリンク */}
-                <div  className="twitterIcon">
-                   <i class="fa-brands fa-twitter" onClick={twitterCloneWeb}></i>
-                </div>
-                
-              </span>
-              <h4 class="my-3">ツイッタークローン</h4>
-              <p class="text-muted">
-                React.jsとfirebaseを使用してツイッターを作成しております。
-                画面レイアウト部分を主としています。
-              </p>
-            </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-
-
-              </span>
-              <h4 class="my-3">作成中</h4>
-              <p class="text-muted">
-                作成中
-              </p>
+            <div className="row text-center">
+              <div className="col-md-4">
+                <span className="fa-stack fa-4x">
+                  <div className="pokekemonIcon">
+                    <i className="fa-brands fa-stack-overflow fa-xl" onClick={pokemonAppWeb}></i>
+                  </div>
+                </span>
+                <h4 className="my-3">ポケモン図鑑</h4>
+                <p className="text-muted">pokemonAPIを使ってデータをReactで表示。</p>
+              </div>
+              <div className="col-md-4">
+                <span className="fa-stack fa-4x">
+                  <div className="twitterIcon">
+                    <i className="fa-brands fa-twitter" onClick={twitterCloneWeb}></i>
+                  </div>
+                </span>
+                <h4 className="my-3">ツイッタークローン</h4>
+                <p className="text-muted">React.jsとFirebaseで作成。</p>
+              </div>
+              <div className="col-md-4">
+                <span className="fa-stack fa-4x"></span>
+                <h4 className="my-3">作成中</h4>
+                <p className="text-muted">作成中</p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-
-      <section id="skill">
-        <div class="text-center">
-          <h2 class="title">スキル</h2>
-          <div class="row text-center">
-            <div class="col-md-4 services">
-              <img src={reactImage} />
-              <h4>React</h4>
-              <p>このポートフォリオページはReactで作成しています。</p>
-            </div>
-            <div class="col-md-4 services">
-              <img src={jsImage} />
-              <h4>HTML/CSS</h4>
-              <p>HTML/CSSがつかえます</p>
-            </div>
-            <div class="col-md-4 services">
-              <img src={firebaseImage} />
-              <h4>Firebase</h4>
-              <p>Firebaseがつかえます</p>
-            </div>
-          </div>
-          <button type="button" class="btn btn-primary">
-            スキル一覧
-          </button>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
---
+++ src/particle/ParticleBackground.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+const ParticlesBackground = () => {
+  useEffect(() => {
+    const loadParticles = () => {
+      if (window.particlesJS) {
+        window.particlesJS.load('particles-js', '/particles.json', function () {
+          console.log('particles.js loaded - callback');
+        });
+      } else {
+        console.error("particles.js is not loaded, retrying...");
+        setTimeout(loadParticles, 1000); // 1秒後にリトライ
+      }
+    };
+
+    loadParticles();
+  }, []);
+
+  return <div id="particles-js" style={{ position: "absolute", width: "100%", height: "100%", zIndex: 0 }}></div>;
+};
+
+export default ParticlesBackground;
