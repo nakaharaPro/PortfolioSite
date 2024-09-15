95416e1202b01dc7de35cce192a15b8f4cb40b8c
nakahara <marimo09300130@gmail.com>
2024-09-15 22:14:54 +0900

    修正

---
+++ src/particle/ParticleBackground.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+const ParticlesBackground = () => {
+    useEffect(() => {
+        let particlesLoaded = false;
+
+        const loadParticles = () => {
+            if (window.particlesJS && !particlesLoaded) {
+                window.particlesJS.load('particles-js', '/particles.json', function () {
+                    console.log('particles.js loaded - callback');
+                    particlesLoaded = true;
+                });
+            } else if (!particlesLoaded) {
+                console.error("particles.js is not loaded, retrying...");
+                setTimeout(loadParticles, 1000); // 1秒後にリトライ
+            }
+        };
+
+       
+};
+
+export default ParticlesBackground;
+
