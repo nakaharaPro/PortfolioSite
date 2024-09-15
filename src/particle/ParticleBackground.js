ab1fdd7c4e367c2c954d256450dfbf2ef26f317c
nakahara <marimo09300130@gmail.com>
2024-09-15 22:15:46 +0900

    修正

--- src/particle/ParticleBackground.js
+++ src/particle/ParticleBackground.js
@@ -16,8 +16,33 @@ const ParticlesBackground = () => {
             }
         };
 
-       
+        loadParticles();
+
+        // クリーンアップ関数: パーティクルの再生成を防ぐため、画面がアンマウントされる際にクリーンアップ(メモリ負荷軽減)
+        return () => {
+            const particlesElement = document.getElementById('particles-js');
+            if (particlesElement && particlesElement.hasChildNodes()) {
+                while (particlesElement.firstChild) {
+                    particlesElement.removeChild(particlesElement.firstChild);
+                }
+                particlesLoaded = false;  // 次回ロード時にリセット
+            }
+        };
+    }, []);
+
+    return (
+        <div id="particles-js" style={{
+            position: 'fixed', // スクロールに対応するように fixed
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            zIndex: 0, // 背景に設定
+            pointerEvents: 'none', // クリックなどのイベントを無効化
+        }}></div>
+    );
 };
 
 export default ParticlesBackground;
+
 
