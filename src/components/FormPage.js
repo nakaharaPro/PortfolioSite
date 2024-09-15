f06e72ddd76902b0e69bf8ae8d1e4d84aa9f62f1
nakahara <marimo09300130@gmail.com>
2024-09-15 22:57:16 +0900

    修正

---
+++ src/components/FormPage.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  };
+
+export default ContactForm;
